55d45901045042082bf0271477e783b8c28f95bf
fadjrin <fadjrinfaa@gmail.com>
2018-01-25 11:09:59 +0700

    edit layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,52 +1,105 @@
-@extends('admin-lte::layouts.auth')
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <link rel="icon" type="image/png" href="../assets/img/log.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-@section('content')
-<div class="login-box-body">
-  <p class="login-box-msg">Sign in to start your session</p>
+  <title>Sekretariat</title>
 
-  <form action="{{ route('login') }}" method="POST">
-    {{ csrf_field() }}
-    <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
-      <input id="nip" type="text" class="form-control" name="nip" value="{{ old('nip') }}" placeholder="Email" required autofocus>
-      <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      @if ($errors->has('nip'))
-      <span class="help-block">{{ $errors->first('nip') }}</span>
-      @endif
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/paper-kit.css?v=2.0.1') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/demo.css') }}" rel="stylesheet">
+
+    <!--     Fonts and icons     -->
+  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+  <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    <nav class="navbar navbar-toggleable-md fixed-top navbar-transparent">
+        <div class="container">
+      <div class="navbar-translate">
+              <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-bar"></span>
+          <span class="navbar-toggler-bar"></span>
+          <span class="navbar-toggler-bar"></span>
+              </button>
+              <a class="navbar-brand" href="http://inka.co.id" target="_blank">
+                    <img border="0" alt="" src="../assets/img/cropped-logo3.png" width="50%" height="50%">
+                </a>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+              <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                      {{-- <a href="{{ url('/') }}" class="nav-link"><i class="nc-icon nc-bank"></i> Home</a> --}}
+                      <a href="{{ url('/') }}" class="btn btn-warning btn-round">Home</a>
+                  </li>
+              </ul>
+          </div>
     </div>
-    <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
-      <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
-      <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      @if ($errors->has('password'))
-      <span class="help-block">{{ $errors->first('password') }}</span>
-      @endif
-    </div>
-    <div class="row">
-      <div class="col-xs-8">
-        <div class="checkbox icheck">
-          <label>
-            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} value="1"> Remember Me
-          </label>
+    </nav>
+    <div class="wrapper">
+        <div class="page-header" style="background-image: url('../assets/img/laptop-computer-pxhere.com(convert).jpg');">
+            <div class="filter"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 offset-lg-4 col-sm-6 offset-sm-3">
+                            <div class="card card-register">
+                                <h3 class="title">Sekretariat</h3>
+                            <form action="{{ route('login') }}" method="POST" class="register-form">
+                                {{ csrf_field() }}
+                                <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}" style="border-color: #605ca8; border-style: solid; border-width: 2px; border-radius: 8px;">
+                                    <div class="input-group form-group-no-border">
+                                        <span class="input-group-addon">
+                                            <i class="nc-icon nc-badge"></i>
+                                        </span>
+                                        <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="Username" required autofocus>
+                                    </div>
+                                </div>
+                                  <span class="glyphicon glyphicon-exclamation-sign form-control-feedback"></span>
+                                  @if ($errors->has('email'))
+                                      <span class="help-block" style="color: #f5593d;">{{ $errors->first('email') }}</span>
+                                  @endif
+                                <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}" style="border-color: #605ca8; border-style: solid; border-width: 2px; border-radius: 8px;">
+                                    <div class="input-group form-group-no-border">
+                                        <span class="input-group-addon">
+                                            <i class="nc-icon nc-key-25"></i>
+                                        </span>
+                                        <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
+                                        
+                                    </div>
+                                </div>
+                                  <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                                  @if ($errors->has('password'))
+                                    <span class="help-block" style="color: #f5593d;">{{ $errors->first('password') }}</span>
+                                  @endif
+                                    <button type="submit" class="btn btn-warning btn-block btn-round">Sign In</button>    
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+          <div class="footer register-footer text-center">
+            <h6>&copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by IT Development</h6>
+          </div>
+                </div>
         </div>
-      </div>
-      <!-- /.col -->
-      <div class="col-xs-4">
-        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-      </div>
-      <!-- /.col -->
     </div>
-  </form>
+</body>
 
-  {{-- <div class="social-auth-links text-center">
-    <p>- OR -</p>
-    <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-    Facebook</a>
-    <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-    Google+</a>
-  </div>
-  <!-- /.social-auth-links --> --}}
+<!-- Core JS Files -->
+<script src="{{ asset('assets/js/jquery-3.2.1.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/jquery-ui-1.12.1.custom.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/tether.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/bootstrap.min.js') }}" type="text/javascript"></script>
 
-  {{-- <a href="{{ route('password.request') }}">I forgot my password</a><br>
-  <a href="{{ route('register') }}" class="text-center">Register a new membership</a> --}}
+<!--  Paper Kit Initialization snd functons -->
+<script src="{{ asset('assets/js/paper-kit.js?v=2.0.1') }}"></script>
 
-</div>
-@endsection
+</html>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,77 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="../assets/img/log.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-        <title>Laravel</title>
+    <title>Sekretariat</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/paper-kit.css?v=2.0.1') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/demo.css') }}" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!--     Fonts and icons     -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        {{--  <a href="{{ url('/register') }}">Register</a>  --}}
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-sm">
-                    SISTEM MANAJEMEN DOKUMEN SEKRETARIAT
-                </div>
-
-                {{--  <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>  --}}
+</head>
+<body>
+    <nav class="navbar navbar-toggleable-md fixed-top navbar-transparent" color-on-scroll="300">
+        <div class="container">
+            <div class="navbar-translate">
+                <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                </button>
+                <a class="navbar-brand" href="http://inka.co.id" target="_blank">
+                    <img border="0" alt="" src="../assets/img/cropped-logo3.png" width="50%" height="50%">
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        @if (Auth::check())
+                            {{-- <a href="{{ url('/home') }}" class="nav-link"><i class="nc-icon nc-bank"></i> Home</a> --}}
+                            <a href="{{ url('/home') }}" class="btn btn-warning btn-round">Home</a>
+                        @else
+                            {{-- <a href="{{ url('/login') }}" class="nav-link"><i class="nc-icon nc-circle-10"></i> Login</a> --}}
+                            <a href="{{ url('/login') }}" class="btn btn-warning btn-round">Login</a>
+                            {{-- <a href="{{ url('/register') }}">Register</a> --}}
+                        @endif
+                    </li>
+                </ul>
             </div>
         </div>
-    </body>
+    </nav>
+
+        <div class="page-header" data-parallax="true" style="background-image: url('../assets/img/laptop-computer-pxhere.com(convert).jpg');">
+            <div class="filter"></div>
+            <div class="container">
+                <div class="motto text-center">
+                    <h1 style="font-size: 60pt; font-weight: bold;">Sekretariat</h1>
+                    <h3>PT Industri Kereta Api (Persero)</h3>
+                    <!-- <br />
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="btn btn-outline-neutral btn-round"><i class="fa fa-play"></i>Watch video</a>
+                    <button type="button" class="btn btn-outline-neutral btn-round">Download</button> -->
+                </div>
+            </div>
+        </div>
+</body>
+
+<!-- Core JS Files -->
+<script src="{{ asset('assets/js/jquery-3.2.1.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/jquery-ui-1.12.1.custom.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/tether.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/bootstrap.min.js') }}" type="text/javascript"></script>
+
+<!--  Paper Kit Initialization snd functons -->
+<script src="{{ asset('assets/js/paper-kit.js?v=2.0.1') }}"></script>
+
 </html>
